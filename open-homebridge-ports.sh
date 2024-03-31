d17e8027a31cf9e954a858a30d2ec0e7525b3f4f
cracklincrotch <donnie@apple2pl.us>
2024-03-31 15:52:16 -0300

    Update open-homebridge-ports.sh

--- open-homebridge-ports.sh
+++ open-homebridge-ports.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#
 # Where to save the FirewallD XML configuration file for Homebridge
 pathname=/etc/firewalld/services
 filename=homebridge.xml
@@ -10,18 +11,31 @@ filename=homebridge.xml
 /usr/bin/echo -e '\t\t<description>Homebridge ports</description>' >> $pathname/$filename
 
 #
-# Get a list of ports that are in use by Homebridge and add them to the XML configuration file
+# Get a list of ports that are in use by Homebridge and add them to the XML 
+# configuration file
 
-for i in $(/usr/bin/lsof -nP -iTCP -sTCP:LISTEN | /usr/bin/grep homebr | /usr/bin/grep "TCP \*:" | \
-                /usr/bin/awk '{print $9}' | /usr/bin/cut -b 3- | /usr/bin/sort -n)
+for i in $( /usr/bin/lsof -nP -iTCP -sTCP:LISTEN | /usr/bin/grep homebr | \
+                /usr/bin/grep "TCP \*:" | /usr/bin/awk '{print $9}' | \
+                /usr/bin/cut -b 3- | /usr/bin/sort -n )
 do
   	/usr/bin/echo -e "\t\t<port protocol=\"tcp\" port=\"$i\"/>" >> $pathname/$filename
 done
 
 /usr/bin/echo -e '\t</service>' >> $pathname/$filename
 
+
 #
-# Set the default SELinux security context on the file, set ownership, and reload the firewall configuration
+# Set the default SELinux security context on the file and set proper file mode.
+
 /usr/sbin/restorecon $pathname/$filename
 /usr/bin/chmod 640 $pathname/$filename
-/usr/bin/firewall-cmd --reload
+
+#
+# If this is the first time the script is being run add the homebridge service
+# to firewalld.  Otherwise reload the new firewall configuration.
+
+if [ -f $pathname/$filename ]; then
+        /usr/bin/firewall-cmd --reload
+else
+    	/usr/bin/firewall-cmd --permanent --new-service-from-file=$pathname/$filename
+fi
